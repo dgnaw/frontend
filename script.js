f6ab06ad7d34dcfff20683e61ea0451f1f663aeb
dgnaw <dogianam09072005@gmail.com>
2025-04-28 09:55:55 +0700

    init commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('callApiBtn').addEventListener('click', async () => {
   try {
-    const response = await fetch('https://backend-git-dgnaw-dev.apps.rm3.7wse.p1.openshiftapps.com');
+    const response = await fetch('https://backend-git-dgnaw-dev.apps.rm3.7wse.p1.openshiftapps.com/api');
     if (!response.ok) {
       throw new Error('HTTP error: ' + response.status);
     }
